e82d75fd08b503a75189d151ce7b139521c21a1d
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-07-29 21:04:08 -0300

    footer added

--- src/template/DefaultPage.js
+++ src/template/DefaultPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './styles';
+import Menu from '../components/Menu';
+import Footer from '../components/Footer';
 
 export default function DefaultPage({ children }) {
 	return (
